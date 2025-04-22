50a45b5f2af70ba2b30cf61c03000b5a5acaeafc
younez999 <lazreqyounes45@gmail.com>
2025-04-22 17:03:48 +0800

    first commit

---
+++ src/main/java/com/example/uavdockingmanagementsystem/UavDockingManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.uavdockingmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UavDockingManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UavDockingManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/uavdockingmanagementsystem/UavDockingManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uavdockingmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UavDockingManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
