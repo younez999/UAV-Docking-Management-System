f2f6ac725f26f77dd7d64e0de2bdeea276c09dae
younez999 <lazreqyounes45@gmail.com>
2025-04-23 20:24:33 +0800

    future work for tomorrow

--- src/main/java/com/example/uavdockingmanagementsystem/UavDockingManagementSystemApplication.java
+++ src/main/java/com/example/uavdockingmanagementsystem/UavDockingManagementSystemApplication.java
@@ -9,6 +9,9 @@ public class UavDockingManagementSystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(UavDockingManagementSystemApplication.class, args);
         System.out.println("welcome to the UAV Docking Management System");
-
+        /*
+          i need  to make first a database system to manage UAV  ,
+         simple data base  (AREA ,  UAV ID , ALLOWENCE , DOCKING STATUS, DOCKING DATE
+        */
     }
 }
