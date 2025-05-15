e2b03d67801156e3a75614c8526c8b889b0f2757
younez999 <139119206+younez999@users.noreply.github.com>
2025-05-15 10:24:57 +0800

    Add MySQL Dockerfile and initialization script
    Introduce a `Dockerfile.mysql` to set up a MySQL instance with runtime-configured secrets and a predefined database environment. Include an `init.sql` script to initialize tables and seed data required for the UAV Management System.

---
+++ db/init.sql
@@ -0,0 +1,131 @@
+﻿-- MySQL dump 10.13  Distrib 9.1.0, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: uav_management_system
+-- ------------------------------------------------------
+-- Server version	8.0.41
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `region`
+--
+
+DROP TABLE IF EXISTS `region`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `region` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `region_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `region`
+--
+
+LOCK TABLES `region` WRITE;
+/*!40000 ALTER TABLE `region` DISABLE KEYS */;
+INSERT INTO `region` VALUES (1,'North'),(2,'South'),(3,'East'),(4,'West');
+/*!40000 ALTER TABLE `region` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `regions`
+--
+
+DROP TABLE IF EXISTS `regions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `regions` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `region_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `regions`
+--
+
+LOCK TABLES `regions` WRITE;
+/*!40000 ALTER TABLE `regions` DISABLE KEYS */;
+INSERT INTO `regions` VALUES (1,'Region A'),(2,'Region B'),(3,'Region C'),(4,'Region D');
+/*!40000 ALTER TABLE `regions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `uav`
+--
+
+DROP TABLE IF EXISTS `uav`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `uav` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(6) DEFAULT NULL,
+  `model` varchar(255) DEFAULT NULL,
+  `owner_name` varchar(255) DEFAULT NULL,
+  `rfid_tag` varchar(255) DEFAULT NULL,
+  `status` enum('AUTHORIZED','UNAUTHORIZED') DEFAULT NULL,
+  `updated_at` datetime(6) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `uav`
+--
+
+LOCK TABLES `uav` WRITE;
+/*!40000 ALTER TABLE `uav` DISABLE KEYS */;
+INSERT INTO `uav` VALUES (11,'2025-05-01 01:42:41.040567','ST32',' meituan','D9858A4F','AUTHORIZED','2025-05-14 10:27:19.647923'),(12,'2025-05-01 03:44:09.634659','ST11',' meituan','136E7035','AUTHORIZED','2025-05-01 03:44:09.634659'),(13,'2025-05-01 03:46:20.209470','ST99','taobao','7B363F0B','AUTHORIZED','2025-05-14 10:27:22.485683');
+/*!40000 ALTER TABLE `uav` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `uav_regions`
+--
+
+DROP TABLE IF EXISTS `uav_regions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `uav_regions` (
+  `uav_id` int NOT NULL,
+  `region_id` int NOT NULL,
+  PRIMARY KEY (`uav_id`,`region_id`),
+  KEY `region_id` (`region_id`),
+  CONSTRAINT `uav_regions_ibfk_1` FOREIGN KEY (`uav_id`) REFERENCES `uav` (`id`),
+  CONSTRAINT `uav_regions_ibfk_2` FOREIGN KEY (`region_id`) REFERENCES `regions` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `uav_regions`
+--
+
+LOCK TABLES `uav_regions` WRITE;
+/*!40000 ALTER TABLE `uav_regions` DISABLE KEYS */;
+INSERT INTO `uav_regions` VALUES (12,1),(13,1),(13,2),(11,3),(12,3),(13,4);
+/*!40000 ALTER TABLE `uav_regions` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-05-14 22:50:31
